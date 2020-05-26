ba41d8d5a6fa8d67329263428318d532bf21382d
surajnh143 <64492299+surajnh143@users.noreply.github.com>
2020-05-26 11:36:10 +0530

    Add files via upload
     commit and start a pull request. Learn more about pull requests.

---
+++ consumer_updated/mail/mail.go
@@ -0,0 +1,23 @@
+package mail
+
+import (
+	"net/smtp"
+)
+
+
+func Send(email string,transaction_id string)(er error) {
+	
+	from := "gaurav.sharma.civ17@itbhu.ac.in"
+	password := ""
+	to := email	
+	msg:="Subject: Transaction Successfull\n\n" +
+	 	"Your transaction has been completed.\nTransaction ID:"+transaction_id;
+	
+	err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("", from, password, "smtp.gmail.com"),
+		from, []string{to}, []byte(msg))
+
+	return err;
+	
+
+}
